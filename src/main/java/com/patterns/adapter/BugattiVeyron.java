dbc18cf872f39fe7c77d42bdeb06fd4bb8ac2340
mustafaev05 <shamil.mustafaev@norse.digital>
2021-06-18 15:13:54 +0300

    created Adapter class and added test

---
+++ src/main/java/com/patterns/adapter/BugattiVeyron.java
@@ -0,0 +1,10 @@
+package com.patterns.adapter;
+
+public class BugattiVeyron implements Movable {
+
+    @Override
+    public double getSpeed() {
+        return 268;
+    }
+
+}
---
+++ src/main/java/com/patterns/adapter/Movable.java
@@ -0,0 +1,7 @@
+package com.patterns.adapter;
+
+public interface Movable {
+    // returns speed in MPH
+    double getSpeed();
+
+}
