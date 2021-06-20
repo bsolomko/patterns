11f1314e2268bd1a492f1e4bead8fff01029d6af
mustafaev05 <shamil.mustafaev@norse.digital>
2021-06-20 17:13:03 +0300

    added description about decorator pattern

--- src/main/java/com/patterns/decorator/TreeDecorator.java
+++ src/main/java/com/patterns/decorator/TreeDecorator.java
@@ -1,3 +1,9 @@
+/**
+ * Decorator pattern is used to add new functionality to an existing object without altering its structure.
+ * <p>
+ * This pattern creates a decorator class which wraps the original class
+ * and provides additional functionality keeping class methods signature intact.
+ */
 package com.patterns.decorator;
 
 import lombok.AllArgsConstructor;
